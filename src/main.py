054a670133cec588d0763f87417ece5db2423489
Kacper Kafara <kacperkafara@gmail.com>
2022-09-06 22:29:43 +0200

    refact: move util functions to separate source file

--- src/main.py
+++ src/main.py
@@ -2,52 +2,8 @@ import csv
 import matplotlib.pyplot as plt
 import datetime as dt
 from transaction import Transaction, TransactionDetails
+from util import *
 
-def parse_data(source: str) -> list[Transaction]:
-    records: list[Transaction] = []
-
-    with open("data/bills.csv") as input:
-        input_reader = csv.reader(input)
-        
-        _ = next(input_reader)
-        for record in input_reader:
-            records.append(Transaction(record))
-
-    return records
-
-def sum_netto(transactions: list[Transaction]) -> float:
-    total = 0
-    for transaction in transactions:
-        total += transaction.amount
-
-    return total
-
-    
-def income_net_by_month(transactions: list[Transaction]) -> list[float]:
-    result = [0 for _ in range(12)]
-
-    for t in transactions:
-        result[t.date.month - 1] += t.amount
-
-    return result
-
-def income_gross_by_month(transactions: list[Transaction]) -> list[float]:
-    result = [0 for _ in range(12)]
-    
-    for t in transactions:
-        if t.amount > 0:
-            result[t.date.month - 1] += t.amount
-
-    return result
-
-def expenses_by_month(transactions: list[Transaction]) -> list[float]:
-    result = [0 for _ in range(12)]
-    
-    for t in transactions:
-        if t.amount < 0:
-            result[t.date.month - 1] += t.amount
-
-    return result
 
     
 def main():
@@ -67,10 +23,6 @@ def main():
     ax.plot([i for i in range(1, 12 + 1)], expenses_by_month(data), linestyle="--")
     ax.scatter([i for i in range(1, 12 + 1)], expenses_by_month(data), label="expenses net", linestyle="--")
     ax.legend()
-    plt.show()
-
-
-    
 
 
 if __name__ == "__main__":
---
+++ src/util.py
@@ -0,0 +1,47 @@
+from transaction import Transaction
+
+def parse_data(source: str) -> list[Transaction]:
+    records: list[Transaction] = []
+
+    with open("data/bills.csv") as input:
+        input_reader = csv.reader(input)
+        
+        _ = next(input_reader)
+        for record in input_reader:
+            records.append(Transaction(record))
+
+    return records
+
+def sum_netto(transactions: list[Transaction]) -> float:
+    total = 0
+    for transaction in transactions:
+        total += transaction.amount
+
+    return total
+
+    
+def income_net_by_month(transactions: list[Transaction]) -> list[float]:
+    result = [0 for _ in range(12)]
+
+    for t in transactions:
+        result[t.date.month - 1] += t.amount
+
+    return result
+
+def income_gross_by_month(transactions: list[Transaction]) -> list[float]:
+    result = [0 for _ in range(12)]
+    
+    for t in transactions:
+        if t.amount > 0:
+            result[t.date.month - 1] += t.amount
+
+    return result
+
+def expenses_by_month(transactions: list[Transaction]) -> list[float]:
+    result = [0 for _ in range(12)]
+    
+    for t in transactions:
+        if t.amount < 0:
+            result[t.date.month - 1] += t.amount
+
+    return result
