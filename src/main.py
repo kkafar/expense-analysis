2113897dd46ed744fe8ea3d125ee2fbd8497e774
Kacper Kafara <kacperkafara@gmail.com>
2022-09-07 01:53:07 +0200

    feat: add basic CLI
    Squashed commit of the following:
    
    commit fd9d253042edf9587eb3bdad53bec412396f93dc
    Author: Kacper Kafara <kacperkafara@gmail.com>
    Date:   Wed Sep 7 01:52:40 2022 +0200
    
        feat: add basic cli options
    
    commit 100a5d7845884d3549ea77d662519115ee91e87f
    Author: Kacper Kafara <kacperkafara@gmail.com>
    Date:   Wed Sep 7 01:48:15 2022 +0200
    
        chore: exclude local env from repo

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import argparse
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdt
 import datetime as dt
@@ -9,12 +10,13 @@ from pprint import pprint
 
     
 def main():
-    data = parse_data("data/transactions-2020.09.07-2022.09.06.csv")
-    print(f"Number of transactions: {len(data)}")
+    parser = argparse.ArgumentParser(description="Small CLI driven application for keeping expenses under control.")
+    parser.add_argument("--file", action='store', default="data/transactions-2020.09.07-2022.09.06.csv")
+    parser.add_argument("--plot", action='store_true')
+    parser.add_argument("--quiet", action='store_true')
+    args = parser.parse_args()
 
-    fig, ax = plt.subplots(figsize=(12.7, 7))
-    
-    ax.set(xlabel="month", ylabel="amount", title="Income & expenses")
+    data = parse_data(args.file)
     
     # For now we assume that thransactions are sorted - the newest at the top
     date_begin = data[-1].date
@@ -36,22 +38,28 @@ def main():
     total_gross = np.sum(income_monthly_gross)
     total_expense = np.sum(expenses_monthly)
     
-    print(f"Total net: {total_net:.2f}")
-    print(f"{'DATE':<12}{'GROSS':>10} {'EXPENSE':>10} {'NET':>10}")
-    for (date, income_gross, expense, income_net) in zip(xdata, income_monthly_gross, expenses_monthly, income_monthly_net):
-        print(f"{date}: {income_gross:>10.2f} {expense:>10.2f} {income_net:>10.2f}")
-    print(f"{'TOTAL':<12}{total_gross:>10.2f} {total_expense:>10.2f} {total_net:>10.2f}")
+    if not args.quiet:
+        print(f"Number of transactions: {len(data)}")
+        print(f"Total net: {total_net:.2f}")
+        print(f"{'DATE':<12}{'GROSS':>10} {'EXPENSE':>10} {'NET':>10}")
+        for (date, income_gross, expense, income_net) in zip(xdata, income_monthly_gross, expenses_monthly, income_monthly_net):
+            print(f"{date}: {income_gross:>10.2f} {expense:>10.2f} {income_net:>10.2f}")
+        print(f"{'TOTAL':<12}{total_gross:>10.2f} {total_expense:>10.2f} {total_net:>10.2f}")
         
+    if args.plot:
+        fig, ax = plt.subplots(figsize=(12.7, 7))
+        
+        ax.set(xlabel="month", ylabel="amount", title="Income & expenses")
 
-    ax.xaxis.set_major_formatter(mdt.DateFormatter('%Y-%m'))
-    ax.xaxis.set_major_locator(mdt.MonthLocator(interval=1))
-    plot_and_scatter(ax, xdata, income_monthly_net, label="income net", linestyle="--")
-    plot_and_scatter(ax, xdata, income_monthly_gross, label="income gross", linestyle="--")
-    plot_and_scatter(ax, xdata, expenses_monthly, label="expenses net", linestyle="--")
-    ax.plot([xdata[0], xdata[-1]], [0, 0])
-    ax.legend()
-    fig.autofmt_xdate()
-    plt.show()
+        ax.xaxis.set_major_formatter(mdt.DateFormatter('%Y-%m'))
+        ax.xaxis.set_major_locator(mdt.MonthLocator(interval=1))
+        plot_and_scatter(ax, xdata, income_monthly_net, label="income net", linestyle="--")
+        plot_and_scatter(ax, xdata, income_monthly_gross, label="income gross", linestyle="--")
+        plot_and_scatter(ax, xdata, expenses_monthly, label="expenses net", linestyle="--")
+        ax.plot([xdata[0], xdata[-1]], [0, 0])
+        ax.legend()
+        fig.autofmt_xdate()
+        plt.show()
 
 
 if __name__ == "__main__":
