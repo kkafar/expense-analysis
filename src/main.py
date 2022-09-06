88b3a74b777f80c83731c5f841fb9701e7f8c509
Kacper Kafara <kacperkafara@gmail.com>
2022-09-06 22:15:46 +0200

    feat: add data parsing & basic graphs

---
+++ src/main.py
@@ -0,0 +1,71 @@
+import csv
+import matplotlib.pyplot as plt
+import datetime as dt
+from transaction import Transaction, TransactionDetails
+
+def parse_data(source: str) -> list[Transaction]:
+    records: list[Transaction] = []
+
+    with open("data/bills.csv") as input:
+        input_reader = csv.reader(input)
+        
+        _ = next(input_reader)
+        for record in input_reader:
+            records.append(Transaction(record))
+
+    return records
+
+def sum_netto(transactions: list[Transaction]) -> float:
+    total = 0
+    for transaction in transactions:
+        total += transaction.amount
+
+    return total
+
+    
+def income_net_by_month(transactions: list[Transaction]) -> list[float]:
+    result = [0 for _ in range(12)]
+
+    for t in transactions:
+        result[t.date.month - 1] += t.amount
+
+    return result
+
+def income_gross_by_month(transactions: list[Transaction]) -> list[float]:
+    result = [0 for _ in range(12)]
+    
+    for t in transactions:
+        if t.amount > 0:
+            result[t.date.month - 1] += t.amount
+
+    return result
+
+def expenses_by_monts(transactions: list[Transaction]) -> list[float]:
+    result = [0 for _ in range(12)]
+    
+    for t in transactions:
+        if t.amount < 0:
+            result[t.date.month - 1] += t.amount
+
+    
+def main():
+    data = parse_data("data/bills.csv")
+
+    # print(Transaction.print_transactions(records=data))
+    print(f"Total net: {sum_netto(data):.2f}")
+    print(f"Expenses by month: {income_net_by_month(data)}")
+
+    fig, ax = plt.subplots(figsize=(12.7, 7))
+    
+    ax.set(xlabel="month", ylabel="amount", title="Income net by month in 2022")
+    ax.plot([i for i in range(1, 12 + 1)], income_net_by_month(data), label="net")
+    ax.plot([i for i in range(1, 12 + 1)], income_gross_by_month(data), label="gross")
+    ax.legend()
+    plt.show()
+
+
+    
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/transaction.py
@@ -0,0 +1,77 @@
+
+from datetime import date
+
+StrOpt = str | None
+
+class TransactionDetails(object):
+    PREFIXES = {
+        "title": "Tytu³: ",
+        "sender": "Nazwa nadawcy: ",
+        "localization": "Lokalizacja: "
+    }
+    KEY_TITLE = "title"
+    KEY_SENDER = "sender"
+    KEY_LOCALIZATION = "localization"
+    
+    
+    def __init__(self, raw_details: list[str]) -> None:
+        self.title: StrOpt = TransactionDetails.__find_detail_and_strip_prefix(
+            TransactionDetails.PREFIXES[TransactionDetails.KEY_TITLE], raw_details)
+        
+        self.sender: StrOpt = TransactionDetails.__find_detail_and_strip_prefix(
+            TransactionDetails.PREFIXES[TransactionDetails.KEY_SENDER], raw_details)
+
+        self.localization: StrOpt = TransactionDetails.__find_detail_and_strip_prefix(
+            TransactionDetails.PREFIXES[TransactionDetails.KEY_LOCALIZATION], raw_details)
+
+        self.other_details: list[str] = list(filter(lambda detail: not any(map(lambda prefix: detail.startswith(prefix), 
+                                                                           TransactionDetails.PREFIXES.values())), raw_details))
+
+    def __str__(self) -> str:
+        return f"Title: {self.title}\nSender: {self.sender}\nOther info:\n  {self.__format_other_info()}"
+
+    def __format_other_info(self) -> str:
+        return "\n  ".join(filter(lambda info: info, self.other_details))
+        
+    @classmethod
+    def __remove_prefix(cls, prefix: str, string: str):
+        return string.removeprefix(prefix) if string is not None else None
+
+    @classmethod
+    def __find_detail_with_prefix(cls, prefix: str, raw_details: list[str]) -> StrOpt:
+        for detail in raw_details: 
+            if detail.startswith(prefix):
+                return detail
+        return None
+
+    @classmethod
+    def __find_detail_and_strip_prefix(cls, prefix: str, raw_details: list[str]) -> StrOpt:
+        return TransactionDetails.__remove_prefix(prefix, TransactionDetails.__find_detail_with_prefix(prefix, raw_details))
+
+
+class Transaction(object):
+    KEY_DATE = 0
+    KEY_TYPE = 2
+    KEY_AMOUNT = 3 
+    KEY_BALANCE_AFTER = 5
+    KEY_DESCRIPTION_START = 6
+    KEY_DESCRIPTION_END = 10
+
+    def __init__(self, raw_record: list[str]) -> 'Transaction':
+        self.date: date = date.fromisoformat(raw_record[Transaction.KEY_DATE])
+        self.type: str = raw_record[Transaction.KEY_TYPE]
+        self.amount: float = float(raw_record[Transaction.KEY_AMOUNT])
+        self.balance_after: float = float(raw_record[Transaction.KEY_BALANCE_AFTER])
+        self.details = TransactionDetails(raw_record[Transaction.KEY_DESCRIPTION_START : -1])
+
+        
+    def __str__(self) -> str:
+        return f"Date: {self.date}\nAmount: {self.amount}\nBalance: {self.balance_after}\n{self.details}"
+
+            
+    @classmethod
+    def print_transactions(cls, records: list['Transaction']) -> None:
+        print('-' * 20)
+        for record in records:
+            print(record)
+            print('-' * 20)
