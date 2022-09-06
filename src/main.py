9d9d8c9283cefec016ab54d999e8bb2c496f8c7f
Kacper Kafara <kacperkafara@gmail.com>
2022-09-07 00:47:17 +0200

    feat: print data table in console

--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdt
 import datetime as dt
+import numpy as np
 from transaction import Transaction, TransactionDetails
 from util import *
 from plot_util import *
+from pprint import pprint
 
     
 def main():
     data = parse_data("data/transactions-2020.09.07-2022.09.06.csv")
     print(f"Number of transactions: {len(data)}")
-    print(data[-1])
 
     fig, ax = plt.subplots(figsize=(12.7, 7))
     
@@ -28,13 +29,20 @@ def main():
         year = date_begin.year + year_oveflow
         xdata.append(dt.date(year, month, 1))
 
-    print(f"Total net: {sum_netto(data):.2f}")
-    print(f"Income net by month: {income_net_by_month(data, span)}")
+    total_net = sum_netto(data)
+    income_monthly_net = np.array(income_net_by_month(data, span))
+    income_monthly_gross = np.array(income_gross_by_month(data, span))
+    expenses_monthly = np.array(expenses_by_month(data, span))
+    
+    print(f"Total net: {total_net:.2f}")
+    for (date, income_gross, expense, income_net) in zip(xdata, income_monthly_gross, expenses_monthly, income_monthly_net):
+        print(f"{date}: {income_gross:>8.2f} {expense:>8.2f} {income_net:>8.2f}")
+        
 
     ax.xaxis.set_major_formatter(mdt.DateFormatter('%Y-%m-%d'))
-    plot_and_scatter(ax, xdata, income_net_by_month(data, span), label="income net", linestyle="--")
-    plot_and_scatter(ax, xdata, income_gross_by_month(data, span), label="income gross", linestyle="--")
-    plot_and_scatter(ax, xdata, expenses_by_month(data, span), label="expenses net", linestyle="--")
+    plot_and_scatter(ax, xdata, income_monthly_net, label="income net", linestyle="--")
+    plot_and_scatter(ax, xdata, income_monthly_gross, label="income gross", linestyle="--")
+    plot_and_scatter(ax, xdata, expenses_monthly, label="expenses net", linestyle="--")
     ax.plot([xdata[0], xdata[-1]], [0, 0])
     ax.legend()
     fig.autofmt_xdate()
