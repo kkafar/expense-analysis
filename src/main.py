037d3f098b35f4fe5d62794f80aabe1a4db652c5
Kacper Kafara <kacperkafara@gmail.com>
2022-09-06 23:54:00 +0200

    feat: allow for any data period
    Squashed commit of the following:
    
    commit b7756b0c769e91de7b5bb80a8bc7c3b16a0010af
    Author: Kacper Kafara <kacperkafara@gmail.com>
    Date:   Tue Sep 6 23:53:13 2022 +0200
    
        feat: allow for any period in main
    
    commit cfd53ac633088c695fa63a2ada054f067224efae
    Author: Kacper Kafara <kacperkafara@gmail.com>
    Date:   Tue Sep 6 23:24:06 2022 +0200
    
        chore: allow for any time period in main
    
    commit f275f2ac3f347494e2dfae2608de1d00b51b2d3e
    Author: Kacper Kafara <kacperkafara@gmail.com>
    Date:   Tue Sep 6 23:23:45 2022 +0200
    
        fix: timediff calculation
    
    commit 613baef844882ac05490f385ac531821900242e3
    Author: Kacper Kafara <kacperkafara@gmail.com>
    Date:   Tue Sep 6 23:03:42 2022 +0200
    
        chore: add method for calculating time diff in months
    
        Python datetime API allows only to get difference between dates in days
        or shorter units...
    
    commit 7a6a0e60f4b4b4cf1bfd93aea8671e0f4eb81c27
    Author: Kacper Kafara <kacperkafara@gmail.com>
    Date:   Tue Sep 6 23:03:25 2022 +0200
    
        chore: add assertion for none source in parse_data method
    
    commit 87538fb95cf46c2f5076c68f93312fdf27636e0a
    Author: Kacper Kafara <kacperkafara@gmail.com>
    Date:   Tue Sep 6 22:55:33 2022 +0200
    
        style: total -> result
    
        So the convention is similiar in all util functions
    
    commit f27d88906c0dcd3be8fd6155dccaee4d849ea197
    Author: Kacper Kafara <kacperkafara@gmail.com>
    Date:   Tue Sep 6 22:54:45 2022 +0200
    
        chore: move time period for calcualtion to parameter
    
        Earlier it was hardcoded to 12.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdt
 import datetime as dt
 from transaction import Transaction, TransactionDetails
 from util import *
@@ -6,19 +7,36 @@ from plot_util import *
 
     
 def main():
-    data = parse_data("data/transactions-2022.01.01-2022.09.06.csv")
-
-    print(f"Total net: {sum_netto(data):.2f}")
-    print(f"Expenses by month: {income_net_by_month(data)}")
+    data = parse_data("data/transactions-2020.09.07-2022.09.06.csv")
+    print(f"Number of transactions: {len(data)}")
+    print(data[-1])
 
     fig, ax = plt.subplots(figsize=(12.7, 7))
     
     ax.set(xlabel="month", ylabel="amount", title="Income & expenses in 2022")
-    xdata = [i for i in range(1, 12 + 1)]
-    plot_and_scatter(ax, xdata, income_net_by_month(data), label="income net", linestyle="--")
-    plot_and_scatter(ax, xdata, income_gross_by_month(data), label="income gross", linestyle="--")
-    plot_and_scatter(ax, xdata, expenses_by_month(data), label="expenses net", linestyle="--")
+    
+    # For now we assume that thransactions are sorted - the newest at the top
+    date_begin = data[-1].date
+    date_end = data[0].date
+    span = timediff_in_months(date_begin, date_end)
+
+    xdata = []
+    for offset in range(span + 1):
+        month = date_begin.month - 1 + offset
+        year_oveflow = month // 12
+        month = month % 12 + 1
+        year = date_begin.year + year_oveflow
+        xdata.append(dt.date(year, month, 1))
+
+    print(f"Total net: {sum_netto(data):.2f}")
+    print(f"Income net by month: {income_net_by_month(data, span)}")
+
+    ax.xaxis.set_major_formatter(mdt.DateFormatter('%Y-%m-%d'))
+    plot_and_scatter(ax, xdata, income_net_by_month(data, span), label="income net", linestyle="--")
+    plot_and_scatter(ax, xdata, income_gross_by_month(data, span), label="income gross", linestyle="--")
+    plot_and_scatter(ax, xdata, expenses_by_month(data, span), label="expenses net", linestyle="--")
     ax.legend()
+    fig.autofmt_xdate()
     plt.show()
 
 
--- src/util.py
+++ src/util.py
@@ -1,8 +1,15 @@
 import csv
+import datetime as dt
 from transaction import Transaction
+
+def timediff_in_months(date_begin: dt.date, date_end: dt.date) -> int:
+    diff = (date_end.year - date_begin.year) * 12 + date_end.month - date_begin.month
+    assert diff >= 0, "Negative timediff. Assure that date_begin <= date_end"
+    return diff
 
 
 def parse_data(source: str) -> list[Transaction]:
+    assert source is not None, "No source provided"
     records: list[Transaction] = []
 
     with open(source) as input:
@@ -16,37 +23,38 @@ def parse_data(source: str) -> list[Transaction]:
 
 
 def sum_netto(transactions: list[Transaction]) -> float:
-    total = 0
-    for transaction in transactions:
-        total += transaction.amount
+    result = 0
 
-    return total
+    for transaction in transactions:
+        result += transaction.amount
+
+    return result
 
     
-def income_net_by_month(transactions: list[Transaction]) -> list[float]:
-    result = [0 for _ in range(12)]
+def income_net_by_month(transactions: list[Transaction], span: int = 12) -> list[float]:
+    result = [0 for _ in range(span + 1)]
 
     for t in transactions:
-        result[t.date.month - 1] += t.amount
+        result[timediff_in_months(transactions[-1].date, t.date) - 1] += t.amount
 
     return result
 
 
-def income_gross_by_month(transactions: list[Transaction]) -> list[float]:
-    result = [0 for _ in range(12)]
+def income_gross_by_month(transactions: list[Transaction], span: int = 12) -> list[float]:
+    result = [0 for _ in range(span + 1)]
     
     for t in transactions:
         if t.amount > 0:
-            result[t.date.month - 1] += t.amount
+            result[timediff_in_months(transactions[-1].date, t.date) - 1] += t.amount
 
     return result
 
 
-def expenses_by_month(transactions: list[Transaction]) -> list[float]:
-    result = [0 for _ in range(12)]
+def expenses_by_month(transactions: list[Transaction], span: int = 12) -> list[float]:
+    result = [0 for _ in range(span + 1)]
     
     for t in transactions:
         if t.amount < 0:
-            result[t.date.month - 1] += t.amount
+            result[timediff_in_months(transactions[-1].date, t.date) - 1] += t.amount
 
     return result
