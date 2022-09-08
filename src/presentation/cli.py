87dc60b371eb5b704d38829f4c75d44fce07d454
Kacper Kafara <kacperkafara@gmail.com>
2022-09-08 16:06:19 +0200

    chore: do not print total net twice

--- src/presentation/cli.py
+++ src/presentation/cli.py
@@ -12,7 +12,6 @@ def print_result_table(
     total_expense: float) -> None:
     
     print(f"Number of transactions: {total_no_transactions}")
-    print(f"Total net: {total_net:.2f}")
     print(f"{'DATE':<12}{'GROSS':>10} {'EXPENSE':>10} {'NET':>10}")
     for (date, gross, expense, net) in zip(timeline, income_gross, expenses, income_net):
         print(f"{date}: {gross:>10.2f} {expense:>10.2f} {net:>10.2f}")
