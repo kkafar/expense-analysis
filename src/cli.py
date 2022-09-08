58155a17c77500df9f4f4e9d0f0f3d2b0a73ec78
Kacper Kafara <kacperkafara@gmail.com>
2022-09-08 15:08:39 +0200

    feat: add help messages for CLI flags

--- src/cli.py
+++ src/cli.py
@@ -4,9 +4,9 @@ import argparse
 def build_cli() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description="Small CLI driven application for keeping expenses under control.")
     parser.add_argument("--file", action='store', default="data/transactions-2020.09.07-2022.09.07.csv")
-    parser.add_argument("--plot", action='store_true')
-    parser.add_argument("--table", action='store_true')
-    parser.add_argument("--all", action="store_true")
+    parser.add_argument("--plot", action='store_true', help="enable plotting")
+    parser.add_argument("--table", action='store_true', help="print table with monthly summary")
+    parser.add_argument("--all", action="store_true", help="enable all action flags")
     return parser
 
 
